7b21e1eef6bed29383fbafb806bbbfaa4d09b76b
Kyky Gaming <mesmacquetimothey@gmail.com>
2024-10-15 10:31:19 +0200

    FINISHED WORKSHOP

---
+++ script.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Julien"));
+
+console.log(chalk.green("Antoine"));
+
+console.log(chalk.yellow("FANTINE"));
+
+console.log(chalk.red("ALICIA"));
